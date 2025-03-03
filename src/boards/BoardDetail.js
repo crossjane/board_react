46008e0a4fecfe081986cd3d2a9b7793edfe6702
crossjane <cross.jane.kim@gmail.com>
2025-03-03 21:12:38 +0900

    25.03.03 댓글 수정

--- src/boards/BoardDetail.js
+++ src/boards/BoardDetail.js
@@ -9,6 +9,7 @@ function BoardDetail(){
 const navigate = useNavigate();
 const [boards, setBoards] = useState(getBoards);
 const [comment, setComment] = useState("");
+const [selectedBoard, setSelectedBoard] = useState();
 
 //비구조화할당
 const {id} = useParams();
@@ -39,17 +40,35 @@ function changeComment(e){
 // }
 
 //배열을 만들어서 만약 이미 comment 가 있으면 그냥 newComment{} 내용을 push , 만약 없으면? 새로운 배열에 newComment{} 내용 push
+//안되는이유??
+
+// function commentSave(){
+//     const findIndex = getBoards.findIndex((board)=>board.id === Number(id));
+ 
+//     const newComment = {content:comment, nickname:"하하호호호"};
+//      if(!comment && null){
+//             getBoards[findIndex].comment =[];
+//          }
+         
+//      getBoards[findIndex].comment.push(newComment);
+//  setComment("");
+    
+//  }
+
+// return (
+
 
 function commentSave(){
     const findIndex = getBoards.findIndex((board)=>board.id === Number(id));
  
     const newComment = {content:comment, nickname:"하하호호호"};
-     if(comment.length >= 1){
-     getBoards[findIndex].comment.push(newComment);
-         }else{
-             getBoards[findIndex].comment.push([newComment]);
+     if(!getBoards[findIndex].comment){
+            getBoards[findIndex].comment =[];
          }
- setComment("");
+         
+     const updatedBoards = getBoards[findIndex].comment.push(newComment);
+     setSelectedBoard(updatedBoards);
+    setComment("");
     
  }
 
@@ -73,7 +92,22 @@ return (
 
     ))}
 
+
+{/* getBoards에 저장된 댓글들이 불러져와야함->1. getBoards에서 comment 가 있는 댓글들을 모두 불러옴.
+ comment 가 있다면? 모두불러움  모가 잘못된건지모르겠음음 */}
+
 <div className='comment'>
+    <div className='comment-view'>
+        { selectedBoard.comment? selectedBoard.map((comment)=>
+        <>
+            <div>{comment.content}</div>
+            <div>{comment.writer}</div>
+        </>
+        )
+        :
+            null
+        }
+    </div>
     <input
         type="text"
         placeholder='댓글을 입력해주세요.'
